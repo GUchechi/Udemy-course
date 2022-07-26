12cc5d616ea4dc3c8b3522031a84daa36b5a8d0d
GUchechi <godswilluchendu7@gmail.com>
2022-07-27 00:36:12 +0100

    added a feedBackList & a global component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import Header from './Components/Header';
 import FeedBackItem from './Components/FeedBackItem';
+import { useState } from 'react';
+import FeedBackData from './Data/FeedBackData';
 
 function App() {
+  const [feedBack, setFeedback] = useState(FeedBackData)
  
 
   return (
--- src/Components/FeedBackItem.jsx
+++ src/Components/FeedBackItem.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const FeedBackItem = () => {
+  const [rating, setRating] = useState(7)
+  const [text, setText] = useState('This is an example of a feedback item.')
+  const handleClick = () => {
+    setRating(() => {
+      return 50;
+    })
+  }
   return (
     <div className='card'>
-        <div className="num-display">10</div>
+        <div className="num-display">{rating}</div>
         <div className="text-display">
-            This is an example of a feedback item.
+            {text}
         </div>
+        <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
