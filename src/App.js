3be321af453638f4c9d79172988c9288febe3189
GUchechi <godswilluchendu7@gmail.com>
2022-07-26 23:44:09 +0100

    added a feedBackItem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Components/Header';
+import FeedBackItem from './Components/FeedBackItem';
 
 function App() {
  
@@ -8,7 +9,7 @@ function App() {
     <>
     <Header />
     <div className="App">
-      
+      <FeedBackItem />
     </div>  
     </>
   );
