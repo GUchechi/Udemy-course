b0c8afb32db871b1664fb003c328284cdefa832a
GUchechi <godswilluchendu7@gmail.com>
2022-07-27 01:04:29 +0100

    deleted 2 components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
 import Header from './Components/Header';
-import { useState } from 'react';
-import FeedBackData from './Data/FeedBackData';
-import FeedBackList from './Components/FeedBackList';
 
 function App() {
-  const [feedBack, setFeedback] = useState(FeedBackData)
  
 
   return (
     <>
     <Header />
     <div className="App">
-      <FeedBackList feedBack='feedBack'/>
+      <FeedBackItem/>
     </div>  
     </>
   );
