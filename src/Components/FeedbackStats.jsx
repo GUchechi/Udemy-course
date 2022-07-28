984ffd25a7ebac54263b0513e52abad16d5d3d97
GUchechi <godswilluchendu7@gmail.com>
2022-07-28 15:42:07 +0100

    added reducer for iteration

--- src/Components/FeedbackStats.jsx
+++ src/Components/FeedbackStats.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const FeedbackStats = () => {
+const FeedbackStats = ({feedback}) => {
+    // Calculate average ratings
+    let average = feedback.reduce((acc, cur) => {
+        return acc + cur.rating
+    }, 0)/ feedback.length
+    
+    average = average.toFixed(1).replace(/[.,]0$/, '');
   return (
-    <div>FeedbackStats</div>
+    <div className="feedback-stats">
+        <h4>{feedback.length} Reviews</h4>
+        <h4>Average Ratings : {isNaN(average) ? 0 : average}</h4>
+    </div>
   )
 }
 
-export default FeedbackStats
+export default FeedbackStats 
