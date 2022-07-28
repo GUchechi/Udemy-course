199c2ceac935c3f343122e5a22ee90f2da62e178
GUchechi <godswilluchendu7@gmail.com>
2022-07-28 16:09:59 +0100

    added a form

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Shared/Card'
 
 const FeedbackForm = () => {
+    const [text, setText] = useState('');
+    const handleChangeText = (event) => {
+        setText(event.target.value);
+    }
   return (
-    <Card>Form</Card>
+    <Card>
+        <form action="">
+            <h2>How would you rate your service with us?</h2>
+            {/* @todo - rating select component */}
+           <div className="input-group">
+           <input onChange={handleChangeText} type="text" placeholder='Write me a review' />
+           <button type='submit'>Send</button>
+           </div>
+        </form>
+    </Card>
   )
 }
 
