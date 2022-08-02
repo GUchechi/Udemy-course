b68e5868558e164055f36fc66ebf6547c50e3ac8
GUchechi <godswilluchendu7@gmail.com>
2022-08-02 04:56:21 +0100

    added useNavigate

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,12 +1,22 @@
-import {useParams} from 'react-router-dom'
+import {Navigate, useNavigate} from 'react-router-dom'
 import React from 'react'
 
 const Post = () => {
-    const params = useParams()
+    const status = 200;
+    const navigate = useNavigate()
+
+    const onClick = () => {
+        console.log('Hello')
+        navigate('/about')
+    }
+
+    if(status === 404) {
+        return <Navigate to='notFound'/>
+    }
   return (
     <div>
-        <h1>Post {params.id}</h1>
-        <p>Name:{params.name}</p>
+        <h1>Posts</h1>
+        <button onClick={onClick}>Click me!!!</button>
     </div>
   )
 }
