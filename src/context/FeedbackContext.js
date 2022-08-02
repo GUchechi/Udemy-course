decee3b291f7e52c7289ba94bc095ec614417534
GUchechi <godswilluchendu7@gmail.com>
2022-08-02 23:42:30 +0100

    added context api

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -3,9 +3,19 @@ import {createContext, useState} from 'react'
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-  return <FeedbackContext.Provider value={{
-    
-  }}>
-    {children}
-  </FeedbackContext.Provider>
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: 'This item is from context',
+      rating: 10,
+    }
+  ])
+
+  return (
+    <FeedbackContext.Provider value={feedback}>
+      {children}
+    </FeedbackContext.Provider>
+  )
 }
+
+export default FeedbackContext
