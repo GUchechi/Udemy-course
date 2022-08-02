4abc6939a3a88575a939b1ee23368e9418826870
GUchechi <godswilluchendu7@gmail.com>
2022-08-02 03:49:20 +0100

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {v4 as uuidv4} from 'uuid'
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import { useState } from 'react';
 import './App.css';
 import FeedbackForm from './Components/FeedbackForm';
@@ -6,6 +7,7 @@ import FeedbackList from './Components/FeedbackList';
 import FeedbackStats from './Components/FeedbackStats';
 import Header from './Components/Header';
 import FeedBackData from './Data/FeedBackData';
+import AboutPage from './pages/AboutPage';
 function App() {
   const [feedback, setFeedback] = useState(FeedBackData)
 
@@ -21,17 +23,23 @@ function App() {
   }
 
   return (
-    <>
-    <Header />
-    <div className="container">
-      <FeedbackForm handleAdd={addFeedback} />
-      <FeedbackStats feedback={feedback}/>
-      <FeedbackList  
-      feedback={feedback}
-      handleDelete={deleteFeedback}
-      />
-    </div>  
-    </>
+    <Router>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route exact path='/' element={
+            <>
+               <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback}/>
+              <FeedbackList  
+              feedback={feedback}
+              handleDelete={deleteFeedback}/>
+            </>
+          }></Route>
+          <Route path='/about' element={<AboutPage/>}/>
+        </Routes>
+      </div>  
+    </Router>
   );
 }
 
