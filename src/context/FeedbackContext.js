53fa42119998d06cd21c9d1fbe30a7eebc4ab050
GUchechi <godswilluchendu7@gmail.com>
2022-08-06 05:36:40 +0100

    installed serve globally

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,70 +1,95 @@
-import {createContext, useState} from 'react'
-import {v4 as uuidv4} from 'uuid'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
-export const FeedbackProvider = ({children}) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This item is from context',
-      rating: 10,
-    },
-    {
-      id: 1,
-      text: 'This item is from context',
-      rating: 10,
-    },
-    {
-      id: 1,
-      text: 'This item is from context',
-      rating: 10,
-    },
-    {
-      id: 1,
-      text: 'This item is from context',
-      rating: 10,
-    }
-  ])
-
+export const FeedbackProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
-    item:{},
+    item: {},
     edit: false,
   })
 
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
 
-  // deleteFeedback
-  const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete')) {
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
+
+  // Add feedback
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+
+    setFeedback([data, ...feedback])
+  }
+
+  // Delete feedback
+  const deleteFeedback = async (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' })
+
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
-  // editFeedback
-  const editFeedback = (item) => {
-    if(window.confirm('Are you sure you want to edit this item?')) {
-      setFeedbackEdit({
-        item,
-        edit: true,
-      })
-    }
+  // Update feedback item
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    })
+
+    const data = await response.json()
+
+    // NOTE: no need to spread data and item
+    setFeedback(feedback.map((item) => (item.id === id ? data : item)))
+
+    // FIX: this fixes being able to add a feedback after editing
+    // credit to Jose https://www.udemy.com/course/react-front-to-back-2022/learn/lecture/29768200#questions/16462688
+    setFeedbackEdit({
+      item: {},
+      edit: false,
+    })
   }
 
-// Add FeedBack
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback]) 
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    })
   }
 
   return (
-    <FeedbackContext.Provider 
-    value={{
-      feedback,
-      deleteFeedback,
-      addFeedback,
-      editFeedback,
-      feedbackEdit,
-      }}>
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        feedbackEdit,
+        isLoading,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        updateFeedback,
+      }}
+    >
       {children}
     </FeedbackContext.Provider>
   )
