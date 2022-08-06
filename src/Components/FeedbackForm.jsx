8f7b42944616cc86f32479bf4f462aec2eedb2a6
GUchechi <godswilluchendu7@gmail.com>
2022-08-06 05:41:24 +0100

    edited context api

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,65 +1,86 @@
-import React, { useEffect, useState } from 'react'
-import { useContext } from 'react'
-import RatingSelect from './RatingSelect';
-import Button from './Shared/Button';
-import Card from './Shared/Card'
+import { useState, useContext, useEffect } from 'react'
+import RatingSelect from './RatingSelect'
+import Card from './shared/Card'
+import Button from './shared/Button'
 import FeedbackContext from '../context/FeedbackContext'
 
-const FeedbackForm = () => {
-    const {addFeedback, feedbackEdit} = useContext(FeedbackContext);
+function FeedbackForm() {
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext)
 
-    useEffect(() => {
-        if(feedbackEdit.edit === true) {
-            setBtnDisabled(false)
-            setText(feedbackEdit.item.text)
-            setRating(feedbackEdit.item.rating)
-        }
-    }, [feedbackEdit])
-
-    const [text, setText] = useState('');
-    const [btnDisabled, setBtnDisabled] = useState(true)
-    const [rating,setRating] = useState(10)
-    const [message, setMessage] = useState('')
-    const handleChangeText = (event) => {
-        if(text === '') {
-            setBtnDisabled(true);
-            setMessage(null)
-        } else if (text !== '' &&  text.trim().length <= 10) {
-            setMessage('Text must be at least 10 characters long.')
-            setBtnDisabled(true)
-        } else {
-            setMessage(null)
-            setBtnDisabled(false)
-        }
-        setText(event.target.value);
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
     }
+  }, [feedbackEdit])
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        if(text.trim().length > 10) {
-            const newFeedback = {
-                text,
-                rating,
-            }
-           addFeedback(newFeedback)
+  // NOTE: This should be checking input value not state as state won't be the updated value until the next render of the component
 
-            setText('');
-        }
+  // prettier-ignore
+  const handleTextChange = ({ target: { value } }) => { // 👈  get the value
+    if (value === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+      
+  // prettier-ignore
+    } else if (value.trim().length < 10) { // 👈 check for less than 10
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
     }
+    setText(value)
+  }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      }
 
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback)
+      } else {
+        addFeedback(newFeedback)
+      }
+
+      // NOTE: reset to default state after submission
+      setBtnDisabled(true) // 👈  add this line to reset disabled
+      setRating(10) //👈 add this line to set rating back to 10
+      setText('')
+    }
+  }
+
+  // NOTE: pass selected to RatingSelect so we don't need local duplicate state
   return (
     <Card>
-        <form onSubmit={handleSubmit}>
-            <h2>How would you rate your service with us?</h2>
-            <RatingSelect select={(rating) => setRating(rating)} />
-           <div className="input-group">
-           <input onChange={handleChangeText} type="text" placeholder='Write me a review' />
-           <Button type='submit' version='secondary' isDisabled={btnDisabled}>Send</Button>
-           </div>
-           {message && <div className='message'>{message}</div>}
-        </form>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={setRating} selected={rating} />
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            type='text'
+            placeholder='Write a review'
+            value={text}
+          />
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+
+        {message && <div className='message'>{message}</div>}
+      </form>
     </Card>
   )
 }
