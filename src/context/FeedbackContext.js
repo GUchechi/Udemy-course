3cfd01277100b4b4d0cfb6293aca3a131395ce96
GUchechi <godswilluchendu7@gmail.com>
2022-08-07 03:11:15 +0100

    deleted commemt

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -62,7 +62,7 @@ export const FeedbackProvider = ({ children }) => {
     // NOTE: no need to spread data and item
     setFeedback(feedback.map((item) => (item.id === id ? data : item)))
 
-
+    // FIX: this fixes being able to add a feedback after editing
     
     setFeedbackEdit({
       item: {},
