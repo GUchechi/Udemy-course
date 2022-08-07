6d19d6175819cb0e4ff842b66d6992f5e457d08a
GUchechi <godswilluchendu7@gmail.com>
2022-08-07 03:09:25 +0100

    deleted commemt

--- src/Components/FeedbackList.jsx
+++ src/Components/FeedbackList.jsx
@@ -4,8 +4,8 @@ import FeedbackItem from './FeedbackItem'
 
 import FeedbackContext from '../context/FeedbackContext'
 
-
-
+// NOTE: added layout prop for nicer animation
+// https://www.framer.com/docs/animation/#layout-animations
 
 function FeedbackList() {
   const { feedback, isLoading } = useContext(FeedbackContext)
